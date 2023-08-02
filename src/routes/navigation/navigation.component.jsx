759bbd573e41349eec5e72f17d46074a7103079f
burjepaul <burjepaul97@gmail.com>
2023-08-02 18:38:26 +0300

    Video Logo added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -32,7 +32,7 @@ const Navigation = () => {
     if (viewportWidth < 600 ) {
     return (
         <Fragment>
-            <div style={{width:'100%', height:'19vh'}}>
+            <div style={{width:'100%', height:'9vh'}}>
                 <Link className="logo-container" to="/">
 
                 </Link>
